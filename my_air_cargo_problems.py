bc78cba38e538da3712d0c581e78cf66eb1c3c0a
Jing Guan <jingg.cxy@foxmail.com>
2017-10-28 16:54:07 +0800

    fix: pass unittest of part1
    1. `.format` should follow string
    2. `==` can’t verify set
    3. privilege of `-` is higher than `&`

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -48,7 +48,6 @@ class AirCargoProblem(Problem):
             list of Action objects
         """
 
-        # TODO create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
         # concrete actions definition: specific literal action that does not include variables as with the schema
         # for example, the action schema 'Load(c, p, a)' can represent the concrete actions 'Load(C1, P1, SFO)'
         # or 'Load(C2, P2, JFK)'.  The actions for the planning problem must be concrete because the problems in
@@ -64,7 +63,7 @@ class AirCargoProblem(Problem):
                 for p in self.planes:
                     for a in self.airports:
                         precond_pos = [expr("At({}, {})".format(c, a)),
-                                       expr("At({}, {})").format(p, a),
+                                       expr("At({}, {})".format(p, a)),
                                        ]
                         precond_neg = []
                         effect_add = [expr("In({}, {})".format(c, p))]
@@ -85,7 +84,7 @@ class AirCargoProblem(Problem):
                 for p in self.planes:
                     for a in self.airports:
                         precond_pos = [expr("In({}, {})".format(c, p)),
-                                       expr("At({}, {})").format(p, a),
+                                       expr("At({}, {})".format(p, a)),
                                        ]
                         precond_neg = []
                         effect_add = [expr("At({}, {})".format(c, a))]
@@ -131,7 +130,7 @@ class AirCargoProblem(Problem):
         kb = PropKB()
         kb.tell(decode_state(state, self.state_map).pos_sentence())
         for action in self.actions_list:
-            if set(action.precond_pos) == set(kb.clauses) and not (set(action.precond_neg) & set(kb.clauses)):
+            if not (set(action.precond_pos) - set(kb.clauses)) and not (set(action.precond_neg) & set(kb.clauses)):
                 possible_actions.append(action)
         return possible_actions
 
@@ -145,8 +144,8 @@ class AirCargoProblem(Problem):
         :return: resulting state after action
         """
         old_state = decode_state(state, self.state_map)
-        pos = set(old_state.pos) | set(action.effect_add) - set(action.effect_rem)
-        neg = set(old_state.neg) | set(action.effect_rem) - set(action.effect_add)
+        pos = (set(old_state.pos) | set(action.effect_add)) - set(action.effect_rem)
+        neg = (set(old_state.neg) | set(action.effect_rem)) - set(action.effect_add)
         new_state = FluentState(list(pos), list(neg))
         return encode_state(new_state, self.state_map)
 
@@ -187,7 +186,7 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+
         count = 0
         return count
 
